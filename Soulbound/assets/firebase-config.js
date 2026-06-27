d5a6ca5b8d5bd7f9cafa9288033656e469d50de1
robrrdz <85893614+robrrdz@users.noreply.github.com>
2026-06-27 11:47:34 -0600

    Update firebase-config.js

--- Soulbound/assets/firebase-config.js
+++ Soulbound/assets/firebase-config.js
@@ -20,12 +20,6 @@
 //    "Tus apps" crea una app Web (</>) y copia el objeto firebaseConfig aquí abajo.
 // ============================================================
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
